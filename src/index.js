7f0905845a163db273b77f7c24ae4f89d3dc0d7f
m.backi <backimichal@gmail.com>
2023-03-30 16:29:24 +0200

    notiflix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { fetchCountries } from './fetchCountries.js';
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
 
+const DEBOUNCE_DELAY = 300;
 const searchBox = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
@@ -9,10 +10,20 @@ const countryInfo = document.querySelector('.country-info');
 const search = async searchTerm => {
   if (!searchTerm) {
     countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
     return;
   }
+
   try {
     const countries = await fetchCountries(searchTerm);
+
+    if (countries.length > 10) {
+      Notiflix.Notify.failure(
+        'Too many matches found. Please enter a more specific name.'
+      );
+      return;
+    }
+
     const countryItems = countries
       .map(country => `<li>${country.name.common}</li>`)
       .join('');
