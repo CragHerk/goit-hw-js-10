e0ba17694ee5c3d6214cd591b263352a490fe936
m.backi <backimichal@gmail.com>
2023-03-31 20:30:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
-//import { fetchCountries } from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries.js';
 //import { debounce } from 'lodash';
-import Notiflix from 'notiflix';
+//import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const searchInput = document.getElementById('search-box');
