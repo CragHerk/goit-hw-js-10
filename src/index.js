024def8a78548d91348dca3d553b43022527cfcd
m.backi <backimichal@gmail.com>
2023-03-31 20:17:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries.js';
-import { debounce } from 'lodash';
+//import { fetchCountries } from './fetchCountries.js';
+//import { debounce } from 'lodash';
 import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
